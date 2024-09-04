b64112e3d8e46e9f7cd7317ca3dace3a56996007
devadmin <priyanshudevilliers@gmail.com>
2024-09-04 22:16:05 +0000

    managing dependencies

---
+++ 03_beans-DependenciesMngt/src/main/java/com/wp/couple/Person.java
@@ -0,0 +1,19 @@
+package com.wp.couple;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    @Autowired
+    Animal animal;
+
+    public Person(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void playWithAnimal(){
+        animal.play();
+    }
+}
