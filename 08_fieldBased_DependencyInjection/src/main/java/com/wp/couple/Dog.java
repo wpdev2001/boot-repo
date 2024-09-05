e0f366d80f365784849286a6225d563daaeda4b9
devadmin <priyanshudevilliers@gmail.com>
2024-09-05 22:53:35 +0000

    field or property based DI

---
+++ 08_fieldBased_DependencyInjection/src/main/java/com/wp/couple/Dog.java
@@ -0,0 +1,14 @@
+package com.wp.couple;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Dog implements Animal{
+    @Override
+    public void play() {
+        System.out.println("Dog is playing");
+    }
+}
