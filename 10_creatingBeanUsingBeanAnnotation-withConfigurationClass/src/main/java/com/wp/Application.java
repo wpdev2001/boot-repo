756d0fdee3e9a673792e565e2c707aae32597114
devadmin <priyanshudevilliers@gmail.com>
2024-09-11 23:58:37 +0000

    creating bean using @Bean annotation with configuration class

---
+++ 10_creatingBeanUsingBeanAnnotation-withConfigurationClass/src/main/java/com/wp/Application.java
@@ -0,0 +1,25 @@
+package com.wp;
+
+import com.wp.couple.Person;
+import com.wp.couple.Samosa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+//In order to declare the samosa as a bean using bean annotation we have to use @Bean annotation on the method level bcz the bean annotation works on method
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		Person personBean = context.getBean(Person.class);
+		personBean.personMethod();
+
+	}
+
+
+}
