33cf98c24a7158036d71e8c6e7a88f8f85282f82
devadmin <priyanshudevilliers@gmail.com>
2024-10-09 10:10:46 +0000

    Spring without spring boot

---
+++ 14_SpringWithoutBoot/src/main/java/com/wp/beans/UserService.java
@@ -0,0 +1,12 @@
+package com.wp.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    public void saveUser(){
+        System.out.println("User is saved successfully");
+    }
+
+}
