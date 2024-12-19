450c0fe8ddccf5267a8dea85fb125aee0ca92fe4
piyush <priyanshudevilliers@gmail.com>
2024-12-20 00:00:25 +0530

    lombok demo

--- 25_Lombok-SpringJSON-Serialization/src/main/java/com/wp/mvc/Application.java
+++ 25_Lombok-SpringJSON-Serialization/src/main/java/com/wp/mvc/Application.java
@@ -1,5 +1,6 @@
 package com.wp.mvc;
 
+import com.wp.mvc.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		Student s = new Student();
+
+		s.setName("AAA");
+		s.setRno(202);
+		s.setPhone("00000000");
+		s.setDept("CS");
+
+		System.out.println(s);
 	}
 
 }
