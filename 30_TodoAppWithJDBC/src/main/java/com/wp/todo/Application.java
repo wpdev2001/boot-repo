e7ec6a3652a0a07f26e1379369c5e7edf26fc104
test <priyanshudevilliers@gmail.com>
2025-01-22 00:05:38 +0000

    new changes uploaded

--- 30_TodoAppWithJDBC/src/main/java/com/wp/todo/Application.java
+++ 30_TodoAppWithJDBC/src/main/java/com/wp/todo/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -30,16 +31,50 @@ public class Application implements CommandLineRunner {
 //		System.out.println("Application Started:");
 //		JdbcTemplate jdbcTemplate = todoDao.getJdbcTemplate();
 //		logger.info("Template object : {}",jdbcTemplate);
+//==========================================================================
+		//Creating TODO and saving into db
+//		Todo todo = new Todo();
+//		todo.setId(456);
+//		todo.setTitle("This is testing JAVA jdbc");
+//		todo.setContent("It's working ..!!");
+//		todo.setStatus("COMPLETED");
+//		todo.setAddedDate(new Date());
+//		todo.setToDoDate(new Date());
+//
+//		todoDao.saveTodo(todo);
+//==========================================================================
+//		GET Single TODO
+//		Todo singleTodo = todoDao.getSingleTodo(123);
+//		logger.info("SINGLE TODO: {}", singleTodo);
+//==========================================================================
+		//GET ALL TODOS
+//		List<Todo> allTodos = todoDao.getAllTodos();
+//		logger.info("ALL TODOS: {}", allTodos);
 
-		Todo todo = new Todo();
-		todo.setId(123);
-		todo.setTitle("This is testing spring jdbc");
-		todo.setContent("It's working yeeey..!!");
-		todo.setStatus("PENDING");
-		todo.setAddedDate(new Date());
-		todo.setToDoDate(new Date());
+//==========================================================================
 
-		todoDao.saveTodo(todo);
+//		//UPDATE TODO
+//		//Getting the older values in todo by using already created API
+//		Todo singleTodo = todoDao.getSingleTodo(456);
+//		logger.info("TODO BEFORE UPDATE: {}", singleTodo);
+//
+//		//Setting new values to the old Todo object & updating it
+//		singleTodo.setTitle("Learn Spring boot course");
+//		singleTodo.setContent("I have completed the update operation");
+//		singleTodo.setStatus("SUCCESSFULL");
+//		singleTodo.setAddedDate(new Date());
+//		singleTodo.setToDoDate(new Date());
+//		todoDao.updateTodo(456,singleTodo);
+//		logger.info("TODO AFTER UPDATE: {}", singleTodo);
+
+//==========================================================================
+
+//		DELETE SINGLE TODO
+//		todoDao.deleteTodo(456);
+
+//==========================================================================
+//		DELETE MULTIPLE TODOS
+		todoDao.deleteMultipleTodo(new int[]{456,789});
 
 	}
 }
--- 30_TodoAppWithJDBC/src/main/java/com/wp/todo/dao/TodoDao.java
+++ 30_TodoAppWithJDBC/src/main/java/com/wp/todo/dao/TodoDao.java
@@ -1,11 +1,20 @@
 package com.wp.todo.dao;
 
 import com.wp.todo.Model.Todo;
+import com.wp.todo.helper.Helper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Component
@@ -16,6 +25,7 @@ public class TodoDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    //injecting jdbcTemplate dependency using constructor.
     public TodoDao(@Autowired JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
 
@@ -46,8 +56,97 @@ public class TodoDao {
     }
 
     //get single todo from db
-    //get all todo from db
+    public Todo getSingleTodo(int id){
+
+        //Getting the data from the db
+        String query = "select * from Todos WHERE id=?";
+        Map<String, Object> todoData = jdbcTemplate.queryForMap(query, id);
+        logger.info("TODO: {}",todoData);
+
+        //Setting the data to Todo obj in order to return
+        Todo todo = new Todo();
+        todo.setId((int)todoData.get("id"));
+        todo.setTitle((String)todoData.get("title"));
+        todo.setContent((String)todoData.get("content"));
+        todo.setStatus((String) todoData.get("status"));
+
+        //For time we need to handle it in different way so creating helper class
+        //Formating date: In short we are fomating the dates
+        todo.setAddedDate(Helper.parseDate((LocalDateTime) todoData.get("addedDate")));
+        todo.setToDoDate(Helper.parseDate((LocalDateTime) todoData.get("todoDate")));
+
+        return todo;
+    }
+
+    //get all todo from db DECODE IT MORE!!!
+    public List<Todo> getAllTodos(){
+        String query = "Select * from Todos";
+        List<Map<String, Object>> todoInListOfMap = jdbcTemplate.queryForList(query);
+
+        List<Todo> todoList = todoInListOfMap.stream().map((myMap) -> {
+
+            Todo todo = new Todo();
+            todo.setId((int) myMap.get("id"));
+            todo.setTitle((String) myMap.get("title"));
+            todo.setContent((String) myMap.get("content"));
+            todo.setStatus((String) myMap.get("status"));
+
+            //For time we need to handle it in different way so creating helper class
+            //Formating date: In short we are fomating the dates
+            todo.setAddedDate(Helper.parseDate((LocalDateTime) myMap.get("addedDate")));
+            todo.setToDoDate(Helper.parseDate((LocalDateTime) myMap.get("todoDate")));
+
+            return todo;
+
+        }).collect(Collectors.toList());
+
+        return todoList;
+    }
+
     //update todo
-    //delete todo
+    public Todo updateTodo(int oldId, Todo newTodo){
+
+        String query = "update Todos set title=?,content=?,status=?,addedDate=?,todoDate=? WHERE id=?";
+        int update = jdbcTemplate.update(query, newTodo.getTitle(), newTodo.getContent(), newTodo.getStatus(), newTodo.getAddedDate(), newTodo.getToDoDate(), oldId);
+        logger.info("UPDATED: {}",update );
+        newTodo.setId(oldId);
+
+        return newTodo;
+
+    }
+
+    //delete SINGLE todo
+    public void deleteTodo(int id){
+
+        String query = "delete from Todos WHERE id=?";
+        int delete = jdbcTemplate.update(query,id);
+        logger.info("DELETED: {}", delete);
+
+    }
+    
+    //Deleting Multiple Todos --> DO more research about Batch processes
+    public void deleteMultipleTodo(int ids[]){
+        String query = "delete from Todos WHERE id=?";
+
+        int[] ints = jdbcTemplate.batchUpdate(query, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                int id = ids[i];
+                ps.setInt(1, id);
+            }
+
+            @Override
+            public int getBatchSize() {
+                //Give the size here
+                return ids.length;
+            }
+        });
+
+        for(int i : ints){
+            logger.info("DELETED {}", i);
+        }
+
+    }
+    
 
 }
